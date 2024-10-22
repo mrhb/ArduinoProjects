774a8fdeb164b576a72b32ad4f6de2beb2b46fbc
hajjar <mmmhajjar83@gmail.com>
2024-10-22 12:14:04 +0330

    jDate for jalali caklener

---
+++ libraries/JDate/examples/JalaliDateDemo/JalaliDateDemo.ino
@@ -0,0 +1,32 @@
+/*
+ Name:		JDate.ino
+ Created:	6/2/2016 2:32:14 PM
+ Author:	Omid Golmakani
+ https://github.com/omseven/JDate
+*/
+#include "SIM800L-SOLDERED.h" // Include soldered library sof SIM800L breakout
+SIM800L sim800(3,2); // So connect D8 to the TX, D9 to the RX
+#include "JDateLib.h"
+String jdate;            // Jalali Date String
+int JY,JM,JD;            // Jalali Year & Month & Day Intiger
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	Serial.begin(9600);
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	Date now(2017, 12, 23);
+	jdate = now.JDate();
+	JY = now.JYear();
+	JM = now.JMonth();
+	JD = now.JDay();
+	Serial.println(JY);
+	Serial.println(JM);
+	Serial.println(JD);
+	Serial.println(jdate);
+	delay(5000);
+
+}
